e491304f082e7fbdc834cc5eef96db408f7a7624
Joseph Paul Cohen <joseph@josephpcohen.com>
2020-06-17 13:44:32 -0400

    add stats script

--- seedbox_stats.py
+++ seedbox_stats.py
@@ -9,6 +9,27 @@ import urllib
 import re, os
 import json
 
+
+def humanbytes(B):
+    B = float(B)
+    KB = float(1000)
+    MB = float(KB ** 2)
+    GB = float(KB ** 3)
+    TB = float(KB ** 4)
+
+    if B < KB:
+        return '{0} {1}'.format(B,'Bytes' if 0 == B > 1 else 'Byte')
+    elif KB <= B < MB:
+        return '{0:.2f} KB'.format(B/KB)
+    elif MB <= B < GB:
+        return '{0:.2f} MB'.format(B/MB)
+    elif GB <= B < TB:
+        return '{0:.2f} GB'.format(B/GB)
+    elif TB <= B:
+        return '{0:.2f} TB'.format(B/TB)
+
+
+
 print(sys.argv[1])
 
 servers = json.load(open(".seedboxes_logins.json", "r"))
@@ -35,9 +56,11 @@ for server in servers:
 
 
         stats["free_space"] = client.free_space(session.download_dir)
+        stats["free_space_human"] = humanbytes(stats["free_space"])
         stats["version"] = session._fields["version"].value
         stats["torrentCount"] = session._fields["torrentCount"].value
         stats["uploadSpeed"] = session._fields["uploadSpeed"].value
+        stats["uploadSpeed_human"] = "{}/s".format(humanbytes(stats["uploadSpeed"]))
     except Exception as ex:
         stats["error"] = str(ex.original)
     except:
